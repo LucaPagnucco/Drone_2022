807ede44e23fc134c714ba96176985c6f5993809
Carlo Pavon <carlo.pavon@stud.itsaltoadriatico.it>
2022-10-27 12:37:55 +0200

    Aggiunta data

--- client/NetCoreClient/ValueObjects/Speed.cs
+++ client/NetCoreClient/ValueObjects/Speed.cs
@@ -4,11 +4,13 @@
     {
         public string Name { get; private set; }
         public int Value { get; private set; }
+        public DateTime Date { get; private set; }
         
         public Speed(int value)
         {
             Name = "speed";
             this.Value = value;
+            Date = DateTime.Now;
         }
 
     }
