3e26ca3c5e98f5df9ebad173d5181fa9c67a6cc3
ITSCOR1908\Administrator <luca.pagnucco@stud.itsaltoadriatico.it>
2022-10-27 09:24:10 +0200

    Aggiunta posizione

---
+++ client/NetCoreClient/ValueObjects/Latitude.cs
@@ -0,0 +1,15 @@
+namespace NetCoreClient.ValueObjects
+{
+    internal class Latitude
+    {
+        public string Name { get; private set; }
+        public double Value { get; private set; }
+        
+        public Latitude(double value)
+        {
+            Name = "latitude";
+            this.Value = value;
+        }
+
+    }
+}
